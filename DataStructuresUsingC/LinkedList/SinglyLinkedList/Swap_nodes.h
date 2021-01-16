724846263cd7453dcae3df9ff16e5a14d1cf685e
ShivaliBandi <shivbandi3004@gmail.com>
2021-01-16 15:10:53 +0530

    new files commited

---
+++ DataStructuresUsingC/LinkedList/SinglyLinkedList/Swap_nodes.h
@@ -0,0 +1,65 @@
+/*
+LEETCODE:1721. Swapping Nodes in a Linked List
+You are given the head of a linked list, and an integer k.
+
+Return the head of the linked list after swapping the values of the kth node from the beginning and the kth node from the end (the list is 1-indexed).
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5], k = 2
+Output: [1,4,3,2,5]
+Example 2:
+
+Input: head = [7,9,6,6,7,8,3,0,9,5], k = 5
+Output: [7,9,6,6,8,7,3,0,9,5]
+Example 3:
+
+Input: head = [1], k = 1
+Output: [1]
+Example 4:
+
+Input: head = [1,2], k = 1
+Output: [2,1]
+Example 5:
+
+Input: head = [1,2,3], k = 2
+Output: [1,2,3]
+ 
+
+Constraints:
+
+The number of nodes in the list is n.
+1 <= k <= n <= 105
+0 <= Node.val <= 100
+
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* swapNodes(struct ListNode* head, int k){
+    
+    struct ListNode *temp=head,*first=head,*end=head;
+    while(k!=1)
+    {
+        temp=temp->next;
+        k--;
+    }
+    first=temp;
+    while(temp->next!=NULL)
+    {
+        temp=temp->next;
+        end=end->next;
+    }
+    k=first->val;
+    first->val=end->val;
+    end->val=k;
+    return head;
+
+}
---
+++ DataStructuresUsingC/Tree/binary_search_tree/maxDepth.h
@@ -0,0 +1,42 @@
+/*
+LeetCode 104. Maximum Depth of Binary Tree
+Given the root of a binary tree, return its maximum depth.
+
+A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+ 
+
+Example 1:
+
+Input: root = [3,9,20,null,null,15,7]
+Output: 3
+Example 2:
+
+Input: root = [1,null,2]
+Output: 2
+Example 3:
+
+Input: root = []
+Output: 0
+Example 4:
+
+Input: root = [0]
+Output: 1
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 104].
+-100 <= Node.val <= 100
+*/
+#include"binary_search_tree.h"
+
+int maxDepth(struct TreeNode* root){
+    int r=0,l=0;
+    if(root!=NULL)
+    {
+        r=maxDepth(root->right)+1;
+        l=maxDepth(root->left)+1;
+    }
+    return (r>l)?r:l;
+}
